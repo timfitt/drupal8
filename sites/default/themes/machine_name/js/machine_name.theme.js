49fbf163748e3f8e678f9b9d7be30ac9cfd6e0b3
timfitt <developer@timfitt.com>
2018-12-06 14:45:25 +1100

    Add developer sub theme

---
+++ sites/default/themes/machine_name/js/machine_name.theme.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Attaches behaviors for machine_name.
+ */
+
+(function ($) {
+
+  'use strict';
+
+  Drupal.behaviors.DeveloperTheme = {
+    attach: function () {
+    }
+  };
+})(jQuery);
